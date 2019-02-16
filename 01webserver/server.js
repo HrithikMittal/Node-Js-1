82e8736fed6daaa3bdcc014e8edd41e71526887f
Adhikansh Mittal <adhikanshmittal@gmail.com>
2019-02-16 20:43:43 +0530

    add resources

--- 01webserver/server.js
+++ 01webserver/server.js
@@ -3,11 +3,14 @@ const http = require('http')
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
-http.createServer((req, res) => {
+var ser = http.createServer((req, res) => {
     res.writeHead(200, {
         'Content-Type': 'text/plain'
     });
     res.end('Hello World');
-}).listen(port, hostname, () => {
+})
+
+
+ser.listen(port, hostname, () => {
     console.log(`Server is running at http://${hostname}:${port}/`);
 });
