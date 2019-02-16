7c301818101fa2cb4aff46fbab26fd46aeb1b3e1
Adhikansh Mittal <adhikanshmittal@gmail.com>
2019-02-16 21:49:41 +0530

    handling server error responses

--- 02Websiteserver/server.js
+++ 02Websiteserver/server.js
@@ -24,8 +24,34 @@ http.createServer((req, res) => {
     try {
         loadFile = fs.lstatSync(filename);
     } catch (error) {
-
+        res.writeHead(404, {
+            "Content-Type": 'text/plain'
+        });
+        res.write('404 Internal Error');
+        res.end();
+        return;
     }
+
+    if (loadFile.isFile()) {
+        var mimeType = mimetypes[path.extname(filename).split('.').reverse()[0]];
+        res.writeHead(200, {
+            "Content-Type": mimeType
+        });
+        var filestream = fs.createReadStream(filename);
+        filestream.pipe(res);
+    } else if (loadFile.isDirectory()) {
+        res.writeHead(302, {
+            'Location': 'index.html'
+        });
+        res.end();
+    } else {
+        res.writeHead(500, {
+            "Content-Type": 'text/plain'
+        });
+        res.write('500 Internal Error');
+        res.end();
+    }
+
 }).listen(port, portname, () => {
     console.log(`Server is running on server http://${portname}:${port}`);
 });
