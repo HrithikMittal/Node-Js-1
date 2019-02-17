aa2978bc58edfc6f5a12e4b2df75e0a870b3e343
Adhikansh Mittal <adhikanshmittal@gmail.com>
2019-02-17 22:13:53 +0530

    confriguration multer database

--- 07multerupload/app.js
+++ 07multerupload/app.js
@@ -7,8 +7,28 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, '/public/myupload')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+var upload = multer({
+    storage: storage,
+}).single('myimage');
+
+
+
+//set for ejs
+app.set('view engine', "ejs");
+// static folder
+app.use(express.static('./public'));
+
 app.get('/', function (req, res) {
-    res.send("Welcome to uploader");
+    // res.send("Welcome to uploader");
+    res.render("index");
 });
 
 app.listen(port, function (req, res) {
