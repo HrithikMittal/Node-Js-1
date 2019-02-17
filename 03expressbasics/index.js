c97102e659b37e82745c7a8ca4bc694013a8b3b1
Adhikansh Mittal <adhikanshmittal@gmail.com>
2019-02-17 09:58:55 +0530

    add express routes

--- 03expressbasics/index.js
+++ 03expressbasics/index.js
@@ -13,7 +13,35 @@ app.get('/about', function (req, res) {
 
 app.get('/contact', function (req, res) {
     res.send("<ul><li>8077510286</li><li>adhikanshmittal@gmail.com</li><li>440 Q block</li></ul>")
-})
+});
+
+app.post('/login', function (req, res) {
+    res.send("login success");
+});
+
+app.delete('/acc-dele', function (req, res) {
+    res.send("Delete Successfully");
+});
+
+app.get('/random.text', function (req, res) {
+    res.send('random.text')
+});
+
+app.get('/ab?cd', function (req, res) {
+    res.send('ab?cd')
+});
+
+app.get('/ab+cd', function (req, res) {
+    res.send('ab+cd')
+});
+
+app.get('/ab*cd', function (req, res) {
+    res.send('ab*cd')
+});
+
+app.get('/ab(cd)?e', function (req, res) {
+    res.send('ab(cd)?e')
+});
 
 port = process.env.PORT || 3000;
 app.listen(port, function (req, res) {
